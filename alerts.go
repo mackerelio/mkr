3f0d19fda514028d9ed96ac73d0650a40856bff2
Shinji Tanaka <shinji.tanaka@gmail.com>
2015-11-19 20:58:06 +0900

    format alert output to human-readable

--- alerts.go
+++ alerts.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"time"
 
 	"github.com/codegangsta/cli"
+	mkr "github.com/mackerelio/mackerel-client-go"
 	"github.com/mackerelio/mkr/logger"
 )
 
@@ -15,6 +18,9 @@ var commandAlerts = cli.Command{
     Request APIs under "/api/v0/alerts". See http://help-ja.mackerel.io/entry/spec/api/v0 .
 `,
 	Action: doAlertsList,
+	Flags: []cli.Flag{
+		cli.StringFlag{Name: "format, f", Value: "", Usage: "Output format. (human/json)"},
+	},
 	Subcommands: []cli.Command{
 		{
 			Name:        "close",
@@ -31,11 +37,66 @@ var commandAlerts = cli.Command{
 
 func doAlertsList(c *cli.Context) {
 	conffile := c.GlobalString("conf")
+	client := newMackerel(conffile)
 
-	alerts, err := newMackerel(conffile).FindAlerts()
+	alerts, err := client.FindAlerts()
 	logger.DieIf(err)
+	if c.String("format") == "json" {
+		PrettyPrintJSON(alerts)
+		return
+	}
 
-	PrettyPrintJSON(alerts)
+	hostsJSON, err := client.FindHosts(&mkr.FindHostsParam{
+		Statuses: []string{"working", "standby", "poweroff", "maintenance"},
+	})
+	hosts := map[string]*HostFormat{}
+	for _, host := range hostsJSON {
+		format := &HostFormat{
+			ID:            host.ID,
+			Name:          host.Name,
+			Status:        host.Status,
+			RoleFullnames: host.GetRoleFullnames(),
+			IsRetired:     host.IsRetired,
+			CreatedAt:     host.DateStringFromCreatedAt(),
+			IPAddresses:   host.IPAddresses(),
+		}
+		hosts[host.ID] = format
+	}
+
+	monitorsJSON, err := client.FindMonitors()
+	monitors := map[string]*mkr.Monitor{}
+	for _, monitor := range monitorsJSON {
+		monitors[monitor.ID] = monitor
+	}
+
+	const layout = "2006-01-02 15:04:05"
+	for _, alert := range alerts {
+		host := hosts[alert.HostID]
+		hostMsg := ""
+		if host != nil {
+			hostMsg = fmt.Sprintf("%s %s %s", host.Name, host.Status, host.RoleFullnames)
+		}
+		monitor := monitors[alert.MonitorID]
+		monitorMsg := ""
+		if monitor != nil {
+			switch monitor.Type {
+			case "connectivity":
+				monitorMsg = fmt.Sprintf("%s", monitor.Type)
+			case "host":
+				switch alert.Status {
+				case "CRITICAL":
+					monitorMsg = fmt.Sprintf("%s %.2f %s %.2f", monitor.Metric, alert.Value, monitor.Operator, monitor.Critical)
+				case "WARNING":
+					monitorMsg = fmt.Sprintf("%s %.2f %s %.2f", monitor.Metric, alert.Value, monitor.Operator, monitor.Warning)
+				default:
+					monitorMsg = fmt.Sprintf("%s %.2f %s %.2f", monitor.Metric, alert.Value, monitor.Operator, monitor.Critical)
+				}
+			default:
+				monitorMsg = fmt.Sprintf("%s", monitor.Type)
+			}
+		}
+		fmt.Printf("%s %s %8s %s %s\n", alert.ID, time.Unix(alert.OpenedAt, 0).Format(layout), alert.Status, monitorMsg, hostMsg)
+	}
 }
 
 func doAlertsClose(c *cli.Context) {
