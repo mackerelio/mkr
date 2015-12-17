d6e598635c1aafe4c86fb6485ca99e36b7e39bae
Shinji Tanaka <shinji.tanaka@gmail.com>
2015-12-17 17:14:45 +0900

    fix test for alerts

--- alerts_test.go
+++ alerts_test.go
@@ -2,16 +2,26 @@ package main
 
 import (
 	"testing"
+	"time"
 
 	mkr "github.com/mackerelio/mackerel-client-go"
 )
 
 func TestFormatJoinedAlert(t *testing.T) {
-	a := mkr.Alert{}
-	h := mkr.Host{}
-	m := mkr.Monitor{}
-	as := alertSet{&a, &h, &m}
-	answer := " 1970-01-01 09:00:00     []"
+
+	const location = "UTC"
+
+	loc, err := time.LoadLocation(location)
+	if err != nil {
+		loc = time.FixedZone(location, 0*60*60)
+	}
+	time.Local = loc
+
+	a := &mkr.Alert{ID: "123", Type: "connectivity", Status: "critical", HostID: "1234", MonitorID: "12345", OpenedAt: 100}
+	h := &mkr.Host{ID: "1234", Name: "foo", Roles: mkr.Roles{}, Status: "working"}
+	m := &mkr.Monitor{ID: "12345", Type: "connectivity", URL: "http://example.com", Service: "bar"}
+	as := alertSet{a, h, m}
+	answer := "123 1970-01-01 00:01:40 critical connectivity foo working []"
 
 	str := formatJoinedAlert(&as, false)
 	if str != answer {
