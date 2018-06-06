056ea4ee680d5f838513704e801bf55486d5c768
Asato Wakisaka <asato.wakisaka@gmail.com>
2018-06-06 20:00:13 +0900

    implement mkr service create

--- services.go
+++ services.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+
+	mkr "github.com/mackerelio/mackerel-client-go"
 	"github.com/mackerelio/mkr/logger"
 	"gopkg.in/urfave/cli.v1"
 )
@@ -10,16 +13,53 @@ var commandServices = cli.Command{
 	Usage:     "List services",
 	ArgsUsage: "",
 	Description: `
-    List the information of the services.
-    Requests "GET /api/v0/services". See https://mackerel.io/api-docs/entry/services#list.
+    Manipulate services. With no subcommand specified, this will list the information of the services.
+    Requests APIs under "/api/v0/services". See https://mackerel.io/api-docs/entry/services .
 `,
-	Action: doServices,
+	Action: doServicesList,
 	Flags:  []cli.Flag{},
+	Subcommands: []cli.Command{
+		{
+			Name:      "create",
+			Usage:     "create a new service",
+			ArgsUsage: "[--memo | -m <memo>] serviceName",
+			Description: `
+    Create a new service with given name.
+    Requests "POST /api/v0/services". See https://mackerel.io/api-docs/entry/services#create .
+`,
+			Action: doServiceCreate,
+			Flags: []cli.Flag{
+				cli.StringFlag{Name: "memo, m", Value: "", Usage: "Memo for the service"},
+			},
+		},
+	},
 }
 
-func doServices(c *cli.Context) error {
+func doServicesList(c *cli.Context) error {
 	services, err := newMackerelFromContext(c).FindServices()
 	logger.DieIf(err)
 	PrettyPrintJSON(services)
+	return nil
+}
+
+func doServiceCreate(c *cli.Context) error {
+	argServiceName := c.Args().Get(0)
+	optMemo := c.String("memo")
+
+	if argServiceName == "" {
+		cli.ShowSubcommandHelp(c)
+		os.Exit(1)
+	}
+
+	client := newMackerelFromContext(c)
+
+	service, err := client.CreateService(&mkr.CreateServiceParam{
+		Name: argServiceName,
+		Memo: optMemo,
+	})
+	logger.DieIf(err)
+
+	logger.Log("created", service.Name)
+
 	return nil
 }
