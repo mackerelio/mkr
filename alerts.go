1ae48676cae0cef91dad5c1ce4cef355e0333023
itchyny <itchyny@hatena.ne.jp>
2018-11-26 21:45:42 +0900

    display all the alerts when -with-closed is not specified

--- alerts.go
+++ alerts.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"regexp"
 	"strings"
@@ -24,7 +25,7 @@ var commandAlerts = cli.Command{
 	Action: doAlertsRetrieve,
 	Flags: []cli.Flag{
 		cli.BoolFlag{Name: "with-closed, w", Usage: "Display open alert including close alert. default: false"},
-		cli.IntFlag{Name: "limit, l", Value: 100, Usage: "Set the number of alerts to display at once when withClosed is active. default: 100"},
+		cli.IntFlag{Name: "limit, l", Value: defaultAlertsLimit, Usage: "Set the number of alerts to display."},
 	},
 	Subcommands: []cli.Command{
 		{
@@ -48,7 +49,7 @@ var commandAlerts = cli.Command{
 				},
 				cli.BoolTFlag{Name: "color, c", Usage: "Colorize output. default: true"},
 				cli.BoolFlag{Name: "with-closed, w", Usage: "Display open alert including close alert. default: false"},
-				cli.IntFlag{Name: "limit, l", Value: 100, Usage: "Set the number of alerts to display at once when withClosed is active. default: 100"},
+				cli.IntFlag{Name: "limit, l", Value: defaultAlertsLimit, Usage: "Set the number of alerts to display."},
 			},
 		},
 		{
@@ -66,6 +67,8 @@ var commandAlerts = cli.Command{
 		},
 	},
 }
+
+const defaultAlertsLimit int = 100
 
 type alertSet struct {
 	Alert   *mkr.Alert
@@ -241,8 +244,7 @@ func formatCheckMessage(msg string) string {
 func doAlertsRetrieve(c *cli.Context) error {
 	client := newMackerelFromContext(c)
 	withClosed := c.Bool("with-closed")
-	limit := c.Int("limit")
-	alerts, err := fetchAlerts(client, withClosed, limit)
+	alerts, err := fetchAlerts(client, withClosed, getAlertsLimit(c, withClosed))
 	logger.DieIf(err)
 	PrettyPrintJSON(alerts)
 	return nil
@@ -253,8 +255,7 @@ func doAlertsList(c *cli.Context) error {
 	filterStatuses := c.StringSlice("host-status")
 	client := newMackerelFromContext(c)
 	withClosed := c.Bool("with-closed")
-	limit := c.Int("limit")
-	alerts, err := fetchAlerts(client, withClosed, limit)
+	alerts, err := fetchAlerts(client, withClosed, getAlertsLimit(c, withClosed))
 	logger.DieIf(err)
 
 	joinedAlerts := joinMonitorsAndHosts(client, alerts)
@@ -294,6 +295,17 @@ func doAlertsList(c *cli.Context) error {
 		fmt.Fprintln(color.Output, formatJoinedAlert(joinAlert, c.BoolT("color")))
 	}
 	return nil
+}
+
+func getAlertsLimit(c *cli.Context, withClosed bool) int {
+	if c.IsSet("limit") {
+		return c.Int("limit")
+	}
+	if withClosed {
+		return defaultAlertsLimit
+	}
+	// When -limit is not set, mkr alerts should print all the open alerts.
+	return math.MaxInt32
 }
 
 func fetchAlerts(client *mkr.Client, withClosed bool, limit int) ([]*mkr.Alert, error) {
