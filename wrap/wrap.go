50a57afad174919a55fb831885bc0bcc88aa0c19
Songmu <y.songmu@gmail.com>
2019-01-13 17:11:11 +0900

    run the wrapping command

--- wrap/wrap.go
+++ wrap/wrap.go
@@ -1,10 +1,16 @@
 package wrap
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
+	"time"
 
+	"github.com/Songmu/wrapcommander"
 	"github.com/mackerelio/mackerel-agent/config"
+	"golang.org/x/sync/errgroup"
 	cli "gopkg.in/urfave/cli.v1"
 )
 
@@ -78,6 +84,95 @@ type app struct {
 	cmd     []string
 }
 
+type result struct {
+	cmd        []string
+	name, memo string
+
+	output, stdout, stderr string
+	pid, exitCode          *int
+	signaled               bool
+	startAt, endAt         time.Time
+
+	msg     string
+	success bool
+}
+
 func (ap *app) run() error {
+	re := ap.runCmd()
+	_ = re
 	return nil
+}
+
+func (ap *app) runCmd() *result {
+	cmd := exec.Command(ap.cmd[0], ap.cmd[1:]...)
+	re := &result{
+		cmd:  ap.cmd,
+		name: ap.name,
+		memo: ap.memo,
+	}
+
+	stdoutPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		re.msg = fmt.Sprintf("command invocation failed with follwing error: %s", err)
+		return re
+	}
+	defer stdoutPipe.Close()
+
+	stderrPipe, err := cmd.StderrPipe()
+	if err != nil {
+		re.msg = fmt.Sprintf("command invocation failed with follwing error: %s", err)
+		return re
+	}
+	defer stderrPipe.Close()
+
+	var (
+		bufStdout = &bytes.Buffer{}
+		bufStderr = &bytes.Buffer{}
+		bufMerged = &bytes.Buffer{}
+	)
+	stdoutPipe2 := io.TeeReader(stdoutPipe, io.MultiWriter(bufStdout, bufMerged))
+	stderrPipe2 := io.TeeReader(stderrPipe, io.MultiWriter(bufStderr, bufMerged))
+
+	re.startAt = time.Now()
+	err = cmd.Start()
+	if err != nil {
+		re.msg = fmt.Sprintf("command invocation failed with follwing error: %s", err)
+		return re
+	}
+	re.pid = &cmd.Process.Pid
+	eg := &errgroup.Group{}
+
+	eg.Go(func() error {
+		defer stdoutPipe.Close()
+		_, err := io.Copy(os.Stdout, stdoutPipe2)
+		return err
+	})
+	eg.Go(func() error {
+		defer stderrPipe.Close()
+		_, err := io.Copy(os.Stderr, stderrPipe2)
+		return err
+	})
+	eg.Wait()
+
+	cmdErr := cmd.Wait()
+	re.endAt = time.Now()
+	ex := wrapcommander.ResolveExitCode(cmdErr)
+	re.exitCode = &ex
+	if *re.exitCode > 128 {
+		w, ok := wrapcommander.ErrorToWaitStatus(cmdErr)
+		if ok {
+			re.signaled = w.Signaled()
+		}
+	}
+	if !re.signaled {
+		re.msg = fmt.Sprintf("command exited with code: %d", *re.exitCode)
+	} else {
+		re.msg = fmt.Sprintf("command died with signal: %d", *re.exitCode&127)
+	}
+	re.stdout = bufStdout.String()
+	re.stderr = bufStderr.String()
+	re.output = bufMerged.String()
+
+	re.success = *re.exitCode == 0
+	return re
 }
