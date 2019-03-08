f088011fa747dda892d4f989b2e9180928dac17d
itchyny <itchyny@hatena.ne.jp>
2019-03-08 16:09:56 +0900

    implement mock client for Mackerel API

---
+++ mackerelclient/mock_client.go
@@ -0,0 +1,46 @@
+package mackerelclient
+
+import mackerel "github.com/mackerelio/mackerel-client-go"
+
+// MockClient represents a mock client of Mackerel API
+type MockClient struct {
+	findHostsCallback func(param *mackerel.FindHostsParam) ([]*mackerel.Host, error)
+}
+
+// MockClientOption represents an option of mock client of Mackerel API
+type MockClientOption func(*MockClient)
+
+// NewMockClient creates a new mock client of Mackerel API
+func NewMockClient(opts ...MockClientOption) *MockClient {
+	client := &MockClient{}
+	for _, opt := range opts {
+		client.ApplyOption(opt)
+	}
+	return client
+}
+
+// ApplyOption applies a mock client option
+func (c *MockClient) ApplyOption(opt MockClientOption) {
+	opt(c)
+}
+
+type errCallbackNotFound string
+
+func (err errCallbackNotFound) Error() string {
+	return string(err) + " callback not found"
+}
+
+// FindHosts ...
+func (c *MockClient) FindHosts(param *mackerel.FindHostsParam) ([]*mackerel.Host, error) {
+	if c.findHostsCallback != nil {
+		return c.findHostsCallback(param)
+	}
+	return nil, errCallbackNotFound("FindHosts")
+}
+
+// MockFindHosts returns an option to set the callback of FindHosts
+func MockFindHosts(callback func(param *mackerel.FindHostsParam) ([]*mackerel.Host, error)) MockClientOption {
+	return func(c *MockClient) {
+		c.findHostsCallback = callback
+	}
+}
