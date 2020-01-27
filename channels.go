34472eb32bac2ba444ef8bab28a84c354048286e
Stefan Utamaru <stefafafan@hatena.ne.jp>
2020-01-27 21:42:40 +0900

    add support for mkr channels pull

--- channels.go
+++ channels.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 
+	"github.com/mackerelio/mackerel-client-go"
 	"github.com/mackerelio/mkr/format"
 	"github.com/mackerelio/mkr/logger"
 	"github.com/mackerelio/mkr/mackerelclient"
@@ -17,6 +20,21 @@ var commandChannels = cli.Command{
 	Requests APIs under "/api/v0/channels". See https://mackerel.io/api-docs/entry/channels .
 	`,
 	Action: doChannelsList,
+	Subcommands: []cli.Command{
+		{
+			Name:      "pull",
+			Usage:     "pull channel settings",
+			ArgsUsage: "[--file-path | -F <file>] [--verbose | -v]",
+			Description: `
+    Pull channels settings from Mackerel server and save them to a file. The file can be specified by filepath argument <file>. The default is 'channels.json'.
+`,
+			Action: doChannelsPull,
+			Flags: []cli.Flag{
+				cli.StringFlag{Name: "file-path, F", Value: "", Usage: "Filename to store channel settings. default: channels.json"},
+				cli.BoolFlag{Name: "verbose, v", Usage: "Verbose output mode"},
+			},
+		},
+	},
 }
 
 func doChannelsList(c *cli.Context) error {
@@ -25,5 +43,46 @@ func doChannelsList(c *cli.Context) error {
 	logger.DieIf(err)
 
 	format.PrettyPrintJSON(os.Stdout, channels)
+	return nil
+}
+
+func doChannelsPull(c *cli.Context) error {
+	isVerbose := c.Bool("verbose")
+	filePath := c.String("file-path")
+
+	channels, err := mackerelclient.NewFromContext(c).FindChannels()
+	logger.DieIf(err)
+
+	channelSaveRules(channels, filePath)
+
+	if isVerbose {
+		format.PrettyPrintJSON(os.Stdout, channels)
+	}
+
+	if filePath == "" {
+		filePath = "channels.json"
+	}
+	logger.Log("info", fmt.Sprintf("Channels are saved to '%s' (%d rules).", filePath, len(channels)))
+	return nil
+}
+
+func channelSaveRules(rules []*mackerel.Channel, optFilePath string) error {
+	filePath := "channels.json"
+	if optFilePath != "" {
+		filePath = optFilePath
+	}
+	file, err := os.Create(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	channels := map[string]interface{}{"channels": rules}
+	data := format.JSONMarshalIndent(channels, "", "    ") + "\n"
+
+	_, err = file.WriteString(data)
+	if err != nil {
+		return err
+	}
 	return nil
 }
