ea27dc8622ec4351651223ab3b9f70fa3fb86440
lufia <lufia@lufia.org>
2021-03-02 11:17:58 +0900

    switch go-get to go-install

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/mackerelio/mkr
 
-go 1.15
+go 1.16
 
 require (
 	github.com/Songmu/prompter v0.4.0
--- tools.go
+++ tools.go
@@ -3,5 +3,6 @@
 package main
 
 import (
+	_ "github.com/Songmu/goxz/cmd/goxz"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 )
