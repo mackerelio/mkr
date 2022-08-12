7f2acc73713be3ac15cb9a3f6fe901b8a90bf7c2
yseto <yseto@users.noreply.github.com>
2022-08-12 15:08:38 +0900

    divide fetch

--- commands.go
+++ commands.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"os"
 	"strings"
 
 	"github.com/Songmu/prompter"
-	"github.com/mackerelio/mackerel-client-go"
 	"github.com/mackerelio/mkr/aws_integrations"
 	"github.com/mackerelio/mkr/channels"
 	"github.com/mackerelio/mkr/checks"
-	"github.com/mackerelio/mkr/format"
+	"github.com/mackerelio/mkr/fetch"
 	"github.com/mackerelio/mkr/hosts"
 	"github.com/mackerelio/mkr/logger"
 	"github.com/mackerelio/mkr/mackerelclient"
@@ -32,7 +30,7 @@ var Commands = []cli.Command{
 	update.Command,
 	throw.Command,
 	metrics.Command,
-	commandFetch,
+	fetch.Command,
 	commandRetire,
 	services.Command,
 	commandMonitors,
@@ -47,24 +45,6 @@ var Commands = []cli.Command{
 	aws_integrations.Command,
 }
 
-var commandFetch = cli.Command{
-	Name:      "fetch",
-	Usage:     "Fetch latest metric values",
-	ArgsUsage: "[--name | -n <metricName>] hostIds...",
-	Description: `
-    Fetch latest metric values about the hosts.
-    Requests "GET /api/v0/tsdb/latest". See https://mackerel.io/api-docs/entry/host-metrics#get-latest .
-`,
-	Action: doFetch,
-	Flags: []cli.Flag{
-		cli.StringSliceFlag{
-			Name:  "name, n",
-			Value: &cli.StringSlice{},
-			Usage: "Fetch metric values identified with <name>. Required. Multiple choices are allowed. ",
-		},
-	},
-}
-
 var commandRetire = cli.Command{
 	Name:      "retire",
 	Usage:     "Retire hosts",
@@ -77,40 +57,6 @@ var commandRetire = cli.Command{
 	Flags: []cli.Flag{
 		cli.BoolFlag{Name: "force", Usage: "Force retirement without confirmation."},
 	},
-}
-
-func split(ids []string, count int) [][]string {
-	xs := make([][]string, 0, (len(ids)+count-1)/count)
-	for i, name := range ids {
-		if i%count == 0 {
-			xs = append(xs, []string{})
-		}
-		xs[len(xs)-1] = append(xs[len(xs)-1], name)
-	}
-	return xs
-}
-
-func doFetch(c *cli.Context) error {
-	argHostIDs := c.Args()
-	optMetricNames := c.StringSlice("name")
-
-	if len(argHostIDs) < 1 || len(optMetricNames) < 1 {
-		cli.ShowCommandHelpAndExit(c, "fetch", 1)
-	}
-
-	allMetricValues := make(mackerel.LatestMetricValues)
-	// Fetches 100 hosts per one request (to avoid URL maximum length).
-	for _, hostIds := range split(argHostIDs, 100) {
-		metricValues, err := mackerelclient.NewFromContext(c).FetchLatestMetricValues(hostIds, optMetricNames)
-		logger.DieIf(err)
-		for key := range metricValues {
-			allMetricValues[key] = metricValues[key]
-		}
-	}
-
-	err := format.PrettyPrintJSON(os.Stdout, allMetricValues)
-	logger.DieIf(err)
-	return nil
 }
 
 func doRetire(c *cli.Context) error {
