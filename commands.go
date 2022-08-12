d35a7a68917f4110f25040f9c16397761967ce5f
yseto <yseto@users.noreply.github.com>
2022-08-12 15:05:03 +0900

    divide metrics

--- commands.go
+++ commands.go
@@ -3,7 +3,6 @@ package main
 import (
 	"os"
 	"strings"
-	"time"
 
 	"github.com/Songmu/prompter"
 	"github.com/mackerelio/mackerel-client-go"
@@ -14,6 +13,7 @@ import (
 	"github.com/mackerelio/mkr/hosts"
 	"github.com/mackerelio/mkr/logger"
 	"github.com/mackerelio/mkr/mackerelclient"
+	"github.com/mackerelio/mkr/metrics"
 	"github.com/mackerelio/mkr/org"
 	"github.com/mackerelio/mkr/plugin"
 	"github.com/mackerelio/mkr/services"
@@ -31,7 +31,7 @@ var Commands = []cli.Command{
 	hosts.CommandCreate,
 	update.Command,
 	throw.Command,
-	commandMetrics,
+	metrics.Command,
 	commandFetch,
 	commandRetire,
 	services.Command,
@@ -45,25 +45,6 @@ var Commands = []cli.Command{
 	checks.Command,
 	wrap.Command,
 	aws_integrations.Command,
-}
-
-var commandMetrics = cli.Command{
-	Name:      "metrics",
-	Usage:     "Fetch metric values",
-	ArgsUsage: "[--host | -H <hostId>] [--service | -s <service>] [--name | -n <metricName>] --from int --to int",
-	Description: `
-    Fetch metric values of 'host metric' or 'service metric'.
-    Requests "/api/v0/hosts/<hostId>/metrics" or "/api/v0/services/<serviceName>/tsdb".
-    See https://mackerel.io/api-docs/entry/host-metrics#get, https://mackerel.io/api-docs/entry/service-metrics#get.
-`,
-	Action: doMetrics,
-	Flags: []cli.Flag{
-		cli.StringFlag{Name: "host, H", Value: "", Usage: "Fetch host metric values of <hostID>."},
-		cli.StringFlag{Name: "service, s", Value: "", Usage: "Fetch service metric values of <service>."},
-		cli.StringFlag{Name: "name, n", Value: "", Usage: "The name of the metric for which you want to obtain the metric."},
-		cli.Int64Flag{Name: "from", Usage: "The first of the period for which you want to obtain the metric. (epoch seconds)"},
-		cli.Int64Flag{Name: "to", Usage: "The end of the period for which you want to obtain the metric. (epoch seconds)"},
-	},
 }
 
 var commandFetch = cli.Command{
@@ -107,37 +88,6 @@ func split(ids []string, count int) [][]string {
 		xs[len(xs)-1] = append(xs[len(xs)-1], name)
 	}
 	return xs
-}
-
-func doMetrics(c *cli.Context) error {
-	optHostID := c.String("host")
-	optService := c.String("service")
-	optMetricName := c.String("name")
-
-	from := c.Int64("from")
-	to := c.Int64("to")
-	if to == 0 {
-		to = time.Now().Unix()
-	}
-
-	client := mackerelclient.NewFromContext(c)
-
-	if optHostID != "" {
-		metricValue, err := client.FetchHostMetricValues(optHostID, optMetricName, from, to)
-		logger.DieIf(err)
-
-		err = format.PrettyPrintJSON(os.Stdout, metricValue)
-		logger.DieIf(err)
-	} else if optService != "" {
-		metricValue, err := client.FetchServiceMetricValues(optService, optMetricName, from, to)
-		logger.DieIf(err)
-
-		err = format.PrettyPrintJSON(os.Stdout, metricValue)
-		logger.DieIf(err)
-	} else {
-		cli.ShowCommandHelpAndExit(c, "metrics", 1)
-	}
-	return nil
 }
 
 func doFetch(c *cli.Context) error {
---
+++ metrics/command.go
@@ -0,0 +1,61 @@
+package metrics
+
+import (
+	"os"
+	"time"
+
+	"github.com/mackerelio/mkr/format"
+	"github.com/mackerelio/mkr/logger"
+	"github.com/mackerelio/mkr/mackerelclient"
+	"github.com/urfave/cli"
+)
+
+var Command = cli.Command{
+	Name:      "metrics",
+	Usage:     "Fetch metric values",
+	ArgsUsage: "[--host | -H <hostId>] [--service | -s <service>] [--name | -n <metricName>] --from int --to int",
+	Description: `
+    Fetch metric values of 'host metric' or 'service metric'.
+    Requests "/api/v0/hosts/<hostId>/metrics" or "/api/v0/services/<serviceName>/tsdb".
+    See https://mackerel.io/api-docs/entry/host-metrics#get, https://mackerel.io/api-docs/entry/service-metrics#get.
+`,
+	Action: doMetrics,
+	Flags: []cli.Flag{
+		cli.StringFlag{Name: "host, H", Value: "", Usage: "Fetch host metric values of <hostID>."},
+		cli.StringFlag{Name: "service, s", Value: "", Usage: "Fetch service metric values of <service>."},
+		cli.StringFlag{Name: "name, n", Value: "", Usage: "The name of the metric for which you want to obtain the metric."},
+		cli.Int64Flag{Name: "from", Usage: "The first of the period for which you want to obtain the metric. (epoch seconds)"},
+		cli.Int64Flag{Name: "to", Usage: "The end of the period for which you want to obtain the metric. (epoch seconds)"},
+	},
+}
+
+func doMetrics(c *cli.Context) error {
+	optHostID := c.String("host")
+	optService := c.String("service")
+	optMetricName := c.String("name")
+
+	from := c.Int64("from")
+	to := c.Int64("to")
+	if to == 0 {
+		to = time.Now().Unix()
+	}
+
+	client := mackerelclient.NewFromContext(c)
+
+	if optHostID != "" {
+		metricValue, err := client.FetchHostMetricValues(optHostID, optMetricName, from, to)
+		logger.DieIf(err)
+
+		err = format.PrettyPrintJSON(os.Stdout, metricValue)
+		logger.DieIf(err)
+	} else if optService != "" {
+		metricValue, err := client.FetchServiceMetricValues(optService, optMetricName, from, to)
+		logger.DieIf(err)
+
+		err = format.PrettyPrintJSON(os.Stdout, metricValue)
+		logger.DieIf(err)
+	} else {
+		cli.ShowCommandHelpAndExit(c, "metrics", 1)
+	}
+	return nil
+}
