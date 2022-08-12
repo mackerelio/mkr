b07c2be056c4008f93a429e19cd0a5776f02626c
yseto <yseto@users.noreply.github.com>
2022-08-12 15:10:43 +0900

    divide retire

--- commands.go
+++ commands.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"strings"
-
-	"github.com/Songmu/prompter"
 	"github.com/mackerelio/mkr/aws_integrations"
 	"github.com/mackerelio/mkr/channels"
 	"github.com/mackerelio/mkr/checks"
 	"github.com/mackerelio/mkr/fetch"
 	"github.com/mackerelio/mkr/hosts"
-	"github.com/mackerelio/mkr/logger"
-	"github.com/mackerelio/mkr/mackerelclient"
 	"github.com/mackerelio/mkr/metrics"
 	"github.com/mackerelio/mkr/org"
 	"github.com/mackerelio/mkr/plugin"
+	"github.com/mackerelio/mkr/retire"
 	"github.com/mackerelio/mkr/services"
 	"github.com/mackerelio/mkr/status"
 	"github.com/mackerelio/mkr/throw"
@@ -31,7 +27,7 @@ var Commands = []cli.Command{
 	throw.Command,
 	metrics.Command,
 	fetch.Command,
-	commandRetire,
+	retire.Command,
 	services.Command,
 	commandMonitors,
 	channels.Command,
@@ -43,46 +39,4 @@ var Commands = []cli.Command{
 	checks.Command,
 	wrap.Command,
 	aws_integrations.Command,
-}
-
-var commandRetire = cli.Command{
-	Name:      "retire",
-	Usage:     "Retire hosts",
-	ArgsUsage: "[--force] hostIds...",
-	Description: `
-    Retire host identified by <hostId>. Be careful because this is an irreversible operation.
-    Requests POST /api/v0/hosts/<hostId>/retire parallelly. See https://mackerel.io/api-docs/entry/hosts#retire .
-`,
-	Action: doRetire,
-	Flags: []cli.Flag{
-		cli.BoolFlag{Name: "force", Usage: "Force retirement without confirmation."},
-	},
-}
-
-func doRetire(c *cli.Context) error {
-	confFile := c.GlobalString("conf")
-	force := c.Bool("force")
-	argHostIDs := c.Args()
-
-	if len(argHostIDs) < 1 {
-		argHostIDs = make([]string, 1)
-		if argHostIDs[0] = mackerelclient.LoadHostIDFromConfig(confFile); argHostIDs[0] == "" {
-			cli.ShowCommandHelpAndExit(c, "retire", 1)
-		}
-	}
-
-	if !force && !prompter.YN("Retire following hosts.\n  "+strings.Join(argHostIDs, "\n  ")+"\nAre you sure?", true) {
-		logger.Log("", "retirement is canceled.")
-		return nil
-	}
-
-	client := mackerelclient.NewFromContext(c)
-
-	for _, hostID := range argHostIDs {
-		err := client.RetireHost(hostID)
-		logger.DieIf(err)
-
-		logger.Log("retired", hostID)
-	}
-	return nil
 }
