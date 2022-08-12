acf087d30bcfbb43b8d9c7324ca1752df8ada397
yseto <yseto@users.noreply.github.com>
2022-08-12 14:56:19 +0900

    divide update

--- commands.go
+++ commands.go
@@ -18,6 +18,7 @@ import (
 	"github.com/mackerelio/mkr/plugin"
 	"github.com/mackerelio/mkr/services"
 	"github.com/mackerelio/mkr/status"
+	"github.com/mackerelio/mkr/update"
 	"github.com/mackerelio/mkr/wrap"
 	"github.com/urfave/cli"
 )
@@ -27,7 +28,7 @@ var Commands = []cli.Command{
 	status.Command,
 	hosts.CommandHosts,
 	hosts.CommandCreate,
-	commandUpdate,
+	update.Command,
 	commandThrow,
 	commandMetrics,
 	commandFetch,
@@ -43,29 +44,6 @@ var Commands = []cli.Command{
 	checks.Command,
 	wrap.Command,
 	aws_integrations.Command,
-}
-
-var commandUpdate = cli.Command{
-	Name:      "update",
-	Usage:     "Update the host",
-	ArgsUsage: "[--name | -n <name>] [--displayName <displayName>] [--status | -st <status>] [--roleFullname | -R <service:role>] [--overwriteRoles | -o] [--memo <memo>] [<hostIds...>]",
-	Description: `
-    Update the host identified with <hostId>.
-    Requests "PUT /api/v0/hosts/<hostId>". See https://mackerel.io/api-docs/entry/hosts#update-information .
-`,
-	Action: doUpdate,
-	Flags: []cli.Flag{
-		cli.StringFlag{Name: "name, n", Value: "", Usage: "Update hostname."},
-		cli.StringFlag{Name: "displayName", Value: "", Usage: "Update displayName."},
-		cli.StringFlag{Name: "status, st", Value: "", Usage: "Update status."},
-		cli.StringSliceFlag{
-			Name:  "roleFullname, R",
-			Value: &cli.StringSlice{},
-			Usage: "Update rolefullname.",
-		},
-		cli.BoolFlag{Name: "overwriteRoles, o", Usage: "Overwrite roles instead of adding specified roles."},
-		cli.StringFlag{Name: "memo", Value: "", Usage: "memo for the Host"},
-	},
 }
 
 var commandMetrics = cli.Command{
@@ -117,85 +95,6 @@ var commandRetire = cli.Command{
 	Flags: []cli.Flag{
 		cli.BoolFlag{Name: "force", Usage: "Force retirement without confirmation."},
 	},
-}
-
-func doUpdate(c *cli.Context) error {
-	confFile := c.GlobalString("conf")
-	argHostIDs := c.Args()
-	optName := c.String("name")
-	optDisplayName := c.String("displayName")
-	optStatus := c.String("status")
-	optRoleFullnames := c.StringSlice("roleFullname")
-	overwriteRoles := c.Bool("overwriteRoles")
-	optMemo := c.String("memo")
-
-	if len(argHostIDs) < 1 {
-		argHostIDs = make([]string, 1)
-		if argHostIDs[0] = mackerelclient.LoadHostIDFromConfig(confFile); argHostIDs[0] == "" {
-			cli.ShowCommandHelpAndExit(c, "update", 1)
-		}
-	}
-
-	needUpdateHostStatus := optStatus != ""
-	needUpdateRolesInHostUpdate := !overwriteRoles && len(optRoleFullnames) > 0
-	needUpdateHost := (optName != "" || optDisplayName != "" || overwriteRoles || optMemo != "" || needUpdateRolesInHostUpdate)
-
-	if !needUpdateHostStatus && !needUpdateHost {
-		logger.Log("update", "at least one argumet is required.")
-		cli.ShowCommandHelpAndExit(c, "update", 1)
-	}
-
-	client := mackerelclient.NewFromContext(c)
-
-	for _, hostID := range argHostIDs {
-		if needUpdateHostStatus {
-			err := client.UpdateHostStatus(hostID, optStatus)
-			logger.DieIf(err)
-		}
-
-		if overwriteRoles {
-			err := client.UpdateHostRoleFullnames(hostID, optRoleFullnames)
-			logger.DieIf(err)
-		}
-
-		if needUpdateHost {
-			host, err := client.FindHost(hostID)
-			logger.DieIf(err)
-			name := ""
-			if optName == "" {
-				name = host.Name
-			} else {
-				name = optName
-			}
-			displayname := ""
-			if optDisplayName == "" {
-				displayname = host.DisplayName
-			} else {
-				displayname = optDisplayName
-			}
-			memo := ""
-			if optMemo == "" {
-				memo = host.Memo
-			} else {
-				memo = optMemo
-			}
-			param := &mackerel.UpdateHostParam{
-				Name:        name,
-				DisplayName: displayname,
-				Meta:        host.Meta,
-				Interfaces:  host.Interfaces,
-				Memo:        memo,
-			}
-			if needUpdateRolesInHostUpdate {
-				param.RoleFullnames = optRoleFullnames
-			}
-			_, err = client.UpdateHost(hostID, param)
-			logger.DieIf(err)
-		}
-
-		logger.Log("updated", hostID)
-	}
-	return nil
 }
 
 func split(ids []string, count int) [][]string {
