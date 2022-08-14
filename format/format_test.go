9169c33d480149cfbacf035695e4a2f9dcb5c340
yseto <yseto@users.noreply.github.com>
2022-08-14 22:21:24 +0900

    added tests.

--- format/format_test.go
+++ format/format_test.go
@@ -1,8 +1,12 @@
 package format
 
 import (
+	"bytes"
+	"encoding/json"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestISO8601Extended(t *testing.T) {
@@ -11,5 +15,86 @@ func TestISO8601Extended(t *testing.T) {
 	got := ISO8601Extended(now)
 	if got != expect {
 		t.Errorf("should be %q got %q", expect, got)
+	}
+}
+
+func TestPrettyPrintJSON(t *testing.T) {
+	testCases := []struct {
+		id       string
+		srcJson  string
+		query    string
+		expected string
+	}{
+		{
+			id:      "simple",
+			srcJson: `{"id": 1, "string": "foo"}`,
+			expected: `{
+    "id": 1,
+    "string": "foo"
+}
+`,
+		},
+		{
+			id:       "simple with query",
+			srcJson:  `{"id": 1, "string": "foo"}`,
+			expected: "1\n",
+			query:    ".id",
+		},
+		{
+			id:      "array",
+			srcJson: `[{"id": 1, "string": "foo"},{"id": 2, "string": "bar"}]`,
+			expected: `[
+    {
+        "id": 1,
+        "string": "foo"
+    },
+    {
+        "id": 2,
+        "string": "bar"
+    }
+]
+`,
+		},
+		{
+			id:       "array with query",
+			srcJson:  `[{"id": 1, "string": "foo"},{"id": 2, "string": "bar"}]`,
+			expected: "1\n2\n",
+			query:    ".[] | .id",
+		},
+		{
+			id:      "array with select object",
+			srcJson: `[{"id": 1, "string": "foo"},{"id": 2, "string": "bar"}]`,
+			expected: `{
+    "id": 2,
+    "string": "bar"
+}
+`,
+			query: ".[] | select(.id == 2)",
+		},
+		{
+			id:       "array with select value",
+			srcJson:  `[{"id": 1, "string": "foo"},{"id": 2, "string": "bar"}]`,
+			expected: "bar\n",
+			query:    ".[] | select(.id == 2) | .string",
+		},
+		{
+			id:      "simple with brackets",
+			srcJson: `{"id": 1, "string": "\u003cfoo\u003e"}`,
+			expected: `{
+    "id": 1,
+    "string": "<foo>"
+}
+`,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.id, func(t *testing.T) {
+			out := new(bytes.Buffer)
+			var src interface{}
+			assert.NoError(t, json.Unmarshal([]byte(tc.srcJson), &src))
+			assert.NoError(t, PrettyPrintJSON(out, src, tc.query))
+			assert.Equal(t, tc.expected, out.String())
+		})
 	}
 }
